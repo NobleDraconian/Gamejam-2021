48749fc66060692e44ee7c5ced752bd9a9dafac6
Jayden Charbonneau <Binarybyte.DEV@gmail.com>
2021-10-09 23:47:01 -0400

    Add lighting configs for test level 1

--- src/Shared/LevelConfigs/TestLevel1.lua
+++ src/Shared/LevelConfigs/TestLevel1.lua
@@ -1,8 +1,35 @@
 local Lighting = game:GetService("Lighting")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local LightingFolder = ReplicatedStorage.Assets.Lighting.TestLevel1
+
 return {
 	LevelOrder = 1,
 	LevelName = "Test Level 1",
-	LightingState = "Default",
 	MapName = "TestLevel1",
-	MusicID = "1836627134"
+	MusicID = "1836627134",
+
+	LightingState = {
+		Properties = {
+			Ambient = Color3.fromRGB(112,112,112),
+			Brightness = 1,
+			ColorShift_Bottom = Color3.fromRGB(0,170,255),
+			ColorShift_Top = Color3.fromRGB(0,170,255),
+			EnvironmentDiffuseScale = 1,
+			EnvironmentSpecularScale = 1,
+			GlobalShadows = true,
+			OutdoorAmbient = Color3.fromRGB(0,0,0),
+			ShadowSoftness = 0,
+			ClockTime = 12.097,
+			GeographicLatitude = 23.999,
+			ExposureCompensation = 0.5
+		},
+		Effects = {
+			Atmosphere = LightingFolder.Atmosphere,
+			Bloom = LightingFolder.Bloom,
+			Blur = LightingFolder.Blur,
+			ColorCorrection = LightingFolder.ColorCorrection,
+			Skybox = LightingFolder.Skybox
+		}
+	}
 }
